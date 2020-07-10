0dbbe3bae13f07a1082e6af2ecea2df22f9987e9
Cédric Meyer <mecedricod@gmail.com>
2020-07-10 12:40:33 +0200

    login activity moredetailed

--- app/src/main/java/eu/cedricmeyer/myapplication/MainActivity.kt
+++ app/src/main/java/eu/cedricmeyer/myapplication/MainActivity.kt
@@ -58,7 +58,7 @@ class MainActivity : AppCompatActivity(), OnAlertOneButtonClickListener {
             }
         })
     }
-    
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
